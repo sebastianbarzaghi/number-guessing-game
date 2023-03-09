1110f7cc43da2b13d75c4d07410491bb3ab6f7bd
sebastianbarzaghi <sebastian.barzaghi@outlook.com>
2023-03-09 21:44:20 +0100

    fix: add HTML and JS code

---
+++ script.js
@@ -0,0 +1,9 @@
+let randomNumber = Math.floor(Math.random() * 100) + 1
+
+let turn = 1
+
+const guesses = document.querySelector(".guesses")
+const lastResult = document.querySelector(".lastResult")
+const lowOrHi = document.querySelector(".lowOrHi")
+
+const input = document.querySelector(".inputField")
