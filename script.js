8821227c5902ec44fc126d7316ebe22d6cb7996e
sebastianbarzaghi <sebastian.barzaghi@outlook.com>
2023-03-09 22:40:50 +0100

    fix: add CSS code and fix JS code

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 let randomNumber = Math.floor(Math.random() * 100) + 1
 
+console.log(randomNumber)
+
 let turn = 1
 
-const guesses = document.querySelector(".guesses")
-const lastResult = document.querySelector(".lastResult")
-const lowOrHi = document.querySelector(".lowOrHi")
+const guesses = document.getElementById("guesses")
+const lastResult = document.getElementById("lastResult")
+const lowOrHi = document.getElementById("lowOrHi")
 
-const input = document.querySelector(".inputField")
+const input = document.getElementById("input")
+const button = document.getElementById("button")
+
+button.addEventListener("click", function() {
+    const inputNumber = input.value;
+    const span = document.createElement("span");
+    const guess = document.createTextNode(inputNumber);
+    
+    span.appendChild(guess);
+    guesses.appendChild(span);
+
+    checkInput(randomNumber, inputNumber)
+
+});
+
+function checkInput(solution, input) {
+    if (solution == input) {
+        const success = document.createTextNode("Congratulations! You got it right!");
+        lastResult.appendChild(success);
+        lastResult.classList.toggle("success");
+    } else {
+        console.log("wrong")
+    }
+}
